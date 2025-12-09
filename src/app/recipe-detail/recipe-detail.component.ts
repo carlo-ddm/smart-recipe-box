3f53be98b1a8b88146b6f1ea79789763338430a6
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2025-12-09 22:01:50 +0100

    feat: implement recipe detail and list components with ingredient adjustment and navigation

--- src/app/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
+import { recipes } from '../mock-recipes';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-detail.component.scss',
 })
 export class RecipeDetailComponent {
+  recipeIndex = input.required<number>();
+  currentRecipe = computed(() => recipes[this.recipeIndex()]);
+  servings = signal<number>(1);
+  adjustedIngredients = computed(() =>
+    this.currentRecipe().ingredients.map((ingredient) => ({
+      ...ingredient,
+      quantity: ingredient.quantity * this.servings(),
+    }))
+  );
 
+  onIncrementServings() {
+    this.servings.update((n) => n + 1);
+  }
+
+  onDecrementServings() {
+    if (this.servings() <= 1) {
+      return;
+    }
+    this.servings.update((n) => n - 1);
+  }
 }
--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,53 +1,38 @@
 import { Component, computed, signal } from '@angular/core';
 import { RecipeModel } from '../models';
 import { recipes } from '../mock-recipes';
+import { RecipeDetailComponent } from '../recipe-detail/recipe-detail.component';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [],
+  imports: [RecipeDetailComponent],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.scss',
 })
 export class RecipeListComponent {
-  recipe = signal<RecipeModel>(recipes[0]);
-  servings = signal<number>(1);
-  adjustedIngredients = computed(() =>
-    this.recipe().ingredients.map((ingredient) => ({
-      ...ingredient,
-      quantity: ingredient.quantity * this.servings(),
-    }))
-  );
+  recipes = signal<RecipeModel[]>(recipes);
+  selectedRecipeIndex = signal<number>(-1);
+
+  selectRecipe(recipeId: number) {
+    this.selectedRecipeIndex.set(recipeId);
+  }
 
   onNextRecipe() {
-    const nextRecipeIndex = recipes.indexOf(this.recipe()) + 1;
-    const totalRecipes = recipes.length;
-
-    if (nextRecipeIndex >= totalRecipes) {
-      this.recipe.set(recipes[0]);
-      return;
-    }
-
-    this.recipe.set(recipes[nextRecipeIndex]);
+    // const nextRecipeIndex = recipes.indexOf(this.recipe()) + 1;
+    // const totalRecipes = recipes.length;
+    // if (nextRecipeIndex >= totalRecipes) {
+    //   this.recipe.set(recipes[0]);
+    //   return;
+    // }
+    // this.recipe.set(recipes[nextRecipeIndex]);
   }
 
   onPrevRecipe() {
-    const previousRecipeIndex = recipes.indexOf(this.recipe()) - 1;
-    if (previousRecipeIndex <= 0) {
-      this.recipe.set(recipes[recipes.length - 1]);
-      return;
-    }
-
-    this.recipe.set(recipes[previousRecipeIndex]);
-  }
-
-  onIncrementServings() {
-    this.servings.update((n) => n + 1);
-  }
-
-  onDecrementServings() {
-    if (this.servings() <= 1) {
-      return;
-    }
-    this.servings.update((n) => n - 1);
+    // const previousRecipeIndex = recipes.indexOf(this.recipe()) - 1;
+    // if (previousRecipeIndex <= 0) {
+    //   this.recipe.set(recipes[recipes.length - 1]);
+    //   return;
+    // }
+    // this.recipe.set(recipes[previousRecipeIndex]);
   }
 }
